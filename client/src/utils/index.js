f20aac3914504d9601ab38abdaaac1aef5e849ca
Lee Kwan Hung <lee.kwanhung0510@gmail.com>
2023-10-21 21:58:00 +0800

    update utils

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,1 +1,33 @@
-export * from "./utils";
+const keyName = "authToken";
+
+export const setAuthToken = token => localStorage.setItem(keyName, token);
+export const getAuthToken = () => localStorage.getItem(keyName);
+export const removeAuthToken = () => localStorage.removeItem(keyName);
+
+export const getRequest = (url) => {
+    return fetcher(url, {});
+};
+
+export const postRequest = (url, data) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetcher(url, options);
+};
+
+const fetcher = (url, options) => {
+    const token = localStorage.getItem(keyName)
+    if (token) {
+        if (!options.headers) {
+            options.headers = {};
+        }
+        options.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return fetch(url, options);
+}
