4c3de5f10764360985fe389d548ddbde429bc00e
Lee Kwan Hung <lee.kwanhung0510@gmail.com>
2023-10-23 23:06:20 +0800

    add strikethrough and set readonly for completed tasks

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,9 +1,10 @@
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
+import styles from './Task.module.css';
 
 export const Task = ({ task, onComplete, onEdit, onDelete }) => {
-    const { task: content, isDone } = task;
+    const { id, task: content, isDone } = task;
 
     return (
         <InputGroup className="my-1">
@@ -12,8 +13,15 @@ export const Task = ({ task, onComplete, onEdit, onDelete }) => {
                     <input type="checkbox" className="form-check-input border-dark" checked={isDone} onChange={(e) => onComplete(e, task)} />
                 </div>
             </span>
-            <Form.Control type="text" className="border-dark" value={content} onChange={(e) => onEdit(e, task)} placeholder="Enter your task" />
-            <Button variant="danger" className="border-dark" onClick={() => onDelete(task.id)}>
+            <Form.Control 
+                type="text" 
+                className={`border-dark ${isDone ? styles.done : ""}`}
+                value={content} 
+                onChange={(e) => onEdit(e, task)} 
+                placeholder="Enter your task" 
+                readOnly={isDone}
+            />
+            <Button variant="danger" className="border-dark" onClick={() => onDelete(id)}>
                 <i className="bi-trash"></i>
             </Button>
         </InputGroup>
