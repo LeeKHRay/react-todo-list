5fbabc2029c56886f1f522847d35a62e479d84db
Lee Kwan Hung <lee.kwanhung0510@gmail.com>
2023-12-17 15:49:18 +0800

    minor change

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,7 +2,7 @@ export const About = () => {
     return (
         <>
             <h1>About</h1>
-            <p>This is a todo list web application.</p>
+            <p>This is a todo list web application developed using React and Nodejs with Express.</p>
         </>
     );
 }
