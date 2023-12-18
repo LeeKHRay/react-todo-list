b9ffa2176d3dfd98ec2be8fe1d9255f94ea5667d
Lee Kwan Hung <lee.kwanhung0510@gmail.com>
2023-12-18 21:30:38 +0800

    update Task schema

--- server/models/Task.js
+++ server/models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    isDone: { type: Boolean, required: true, default: false },
+    isCompleted: { type: Boolean, required: true, default: false },
     priority: { type: Number, required: true },
     username: { type: String, required: true }
 });
