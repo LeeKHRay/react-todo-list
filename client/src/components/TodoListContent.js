246359aab23066d76eb8c53858206ec6f0a6bf73
Lee Kwan Hung <lee.kwanhung0510@gmail.com>
2023-12-22 23:07:56 +0800

    memoize event handlers passed to Task component

--- client/src/components/TodoListContent.js
+++ client/src/components/TodoListContent.js
@@ -4,14 +4,24 @@ import { Task, CustomDragLayer } from "../components";
 import { useTasksContext } from "../contexts/TasksContext";
 
 export const TodoListContent = () => {
-    const { tasks } = useTasksContext();
+    const { tasks, completeTask, editTask, moveTask, deleteTask } = useTasksContext();
 
     return (
         tasks.length > 0 && 
         <DndProvider backend={HTML5Backend}>
             <div style={{padding: "10px", backgroundColor: "#c6f7da", border: "1px solid #20c997", borderRadius: "0.375rem", marginBottom: 70}}>
                 {
-                    tasks.map((task, idx) => <Task key={task.id} task={task} idx={idx} />)
+                    tasks.map((task, idx) => (
+                        <Task
+                            key={task.id} 
+                            task={task} 
+                            idx={idx}
+                            onComplete={completeTask}
+                            onEdit={editTask}
+                            onMove={moveTask}
+                            onDelete={deleteTask}
+                        />
+                    ))
                 }
             </div>
             <CustomDragLayer />
